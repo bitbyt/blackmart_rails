adbb08fc1a2b0a17562900783a58c2829e35e724
Luqman Mohamed <lukas.woodman@gmail.com>
2016-07-27 15:53:14 +0800

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
   resources :categories
   resources :brands
   resources :products
