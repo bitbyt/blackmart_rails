53557f73acb304e4e321a9ae77af46f13e21f8d2
Luqman Mohamed <lukas.woodman@gmail.com>
2016-07-30 15:25:13 +0800

    Completed user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,27 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/contact'
 
-  resources :categories
-  resources :brands
+  # static pages routes, not connected to models
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
+  # dynamic routes
+  get '/signup', to: 'users#new'
+  get '/products', to: 'products#index'
+  get '/products/new', to: 'products#new'
+
+  get '/categories', to: 'categories#index'
+  get '/categories/new', to: 'categories#new'
+
+  # login routes
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  # restful routes
+  resources :users, except: [ :new ]
   resources :products
-  resources :users
+  resources :categories
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
